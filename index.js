f5a44d71df18bb55a270a1fa0b4850d849665f0d
Lien Nguyen <lienlam217@gmail.com>
2022-11-27 17:46:03 -0600

    commit tree 4043f33bf98dbc38ecc6a50ca152ca4ca9513663

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+   const result = someValue - 42
+    return Math.abs(result)  
+  }
+
+function distanceFromHqInFeet(someValue) {
+    const resultInFeet = distanceFromHqInBlocks(someValue)*264
+    return Math.abs(resultInFeet)
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const distance = (destination - start)*264
+    return Math.abs(distance)
+}
+
+function calculatesFarePrice(start, destination) {
+    let fare
+    const distance1 = distanceTravelledInFeet(start, destination) 
+    if (distance1 <= 400) {
+        fare = 0
+    }
+    else if (distance1 < 2000) {
+        fare = (distance1 - 400)*(0.02)
+    }
+    else if (distance1 < 2500) {
+        fare = 25
+    }
+    else if (distance1 > 2500) {
+        fare = 'cannot travel that far'
+    }
+    return fare
+}
+
